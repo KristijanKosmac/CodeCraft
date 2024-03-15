807a03de34bfb837548ae6f2af3bf57bd7634597
Kristijan Kosmach <49007481+KristijanKosmac@users.noreply.github.com>
2024-03-15 11:16:27 +0100

    Add files via upload

---
+++ app/constants/index.ts
@@ -0,0 +1,216 @@
+import {
+    people01,
+    people02,
+    people03,
+    facebook,
+    instagram,
+    linkedin,
+    twitter,
+    airbnb,
+    binance,
+    coinbase,
+    dropbox,
+    send,
+    shield,
+    star,
+  } from "../../public";
+  
+  export const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+    {
+      id: "expertise",
+      title: "Expertise",
+    },
+    {
+      id: "clients",
+      title: "Clients",
+    },
+    {
+      id: "contact-us",
+      title: "Contact us",
+    },
+  ];
+  
+  export const features = [
+    {
+      id: "feature-1",
+      icon: star,
+      title: "Design",
+      content:
+        "Specialize in crafting modern designs with exceptional UI and UX.",
+    },
+    {
+      id: "feature-2",
+      icon: shield,
+      title: "100% Secured",
+      content:
+        "Our mission is to deliver highly secure applications, ensuring your data and assets are protected at all times.",
+    },
+    {
+      id: "feature-3",
+      icon: send,
+      title: "Performance",
+      content:
+        "Engineering high-performance, lightning-fast, and scalable applications.",
+    },
+  ];
+  
+  export const feedback = [
+    {
+      id: "feedback-1",
+      content:
+        "Choosing CodeCraft was one of the best decisions we've made. Their professionalism and expertise have helped us achieve our goals faster than we ever thought possible",
+      name: "David Rodriguez",
+      // title: "Founder & Leader",
+      // img: people01,
+    },
+    {
+      id: "feedback-2",
+      content:
+        "Your team creativity and dedication have made a significant impact on our success",
+      name: "Emily Johnson",
+      // title: "Founder & Leader",
+      img: people03,
+    },
+    {
+      id: "feedback-3",
+      content:
+        "Working with you has been a breath of fresh air. Their team's passion for what they do shines through in every project. We couldn't be happier with the results!",
+      name: "Michael Patel",
+      // title: "Founder & Leader",
+      img: people02,
+    },
+  ];
+  
+  export const stats = [
+    {
+      id: "stats-1",
+      title: "Highly Skilled Programmers",
+      value: "20+",
+    },
+    {
+      id: "stats-2",
+      title: "Successful Projects",
+      value: "10+",
+    },
+    {
+      id: "stats-3",
+      title: "Satisfied clients",
+      value: "10+",
+    },
+  ];
+  
+  export const footerLinks = [
+    {
+      title: "Useful Links",
+      links: [
+        {
+          name: "Content",
+          link: "https://www.hoobank.com/content/",
+        },
+        {
+          name: "How it Works",
+          link: "https://www.hoobank.com/how-it-works/",
+        },
+        {
+          name: "Create",
+          link: "https://www.hoobank.com/create/",
+        },
+        {
+          name: "Explore",
+          link: "https://www.hoobank.com/explore/",
+        },
+        {
+          name: "Terms & Services",
+          link: "https://www.hoobank.com/terms-and-services/",
+        },
+      ],
+    },
+    {
+      title: "Community",
+      links: [
+        {
+          name: "Help Center",
+          link: "https://www.hoobank.com/help-center/",
+        },
+        {
+          name: "Partners",
+          link: "https://www.hoobank.com/partners/",
+        },
+        {
+          name: "Suggestions",
+          link: "https://www.hoobank.com/suggestions/",
+        },
+        {
+          name: "Blog",
+          link: "https://www.hoobank.com/blog/",
+        },
+        {
+          name: "Newsletters",
+          link: "https://www.hoobank.com/newsletters/",
+        },
+      ],
+    },
+    {
+      title: "Partner",
+      links: [
+        {
+          name: "Our Partner",
+          link: "https://www.hoobank.com/our-partner/",
+        },
+        {
+          name: "Become a Partner",
+          link: "https://www.hoobank.com/become-a-partner/",
+        },
+      ],
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: "social-media-1",
+      icon: instagram,
+      link: "https://www.instagram.com/",
+    },
+    {
+      id: "social-media-2",
+      icon: facebook,
+      link: "https://www.facebook.com/",
+    },
+    {
+      id: "social-media-3",
+      icon: twitter,
+      link: "https://www.twitter.com/",
+    },
+    {
+      id: "social-media-4",
+      icon: linkedin,
+      link: "https://www.linkedin.com/",
+    },
+  ];
+  
+  export const clients = [
+    {
+      id: "client-1",
+      logo: airbnb,
+      bgImg: "../assets/airbnb-small.png"
+    },
+    {
+      id: "client-2",
+      logo: binance,
+      bgImg: "../assets/binance-small.png"
+    },
+    {
+      id: "client-3",
+      logo: coinbase,
+      bgImg: "../assets/binance-small.png"
+    },
+    {
+      id: "client-4",
+      logo: dropbox,
+      bgImg: "../assets/dropbox-small.png"
+    },
+  ];
